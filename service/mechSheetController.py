04826ac78514a37f71ebc34e0c03617a19d688cf
ayushspark11 <ayushspark11@gmail.com>
2022-05-20 17:42:48 -0400

    Save mech configs as json file to local storage

--- service/mechSheetController.py
+++ service/mechSheetController.py
@@ -1,5 +1,13 @@
 import json
 import model.mechModel.mech as mechmodel
+import os
+
+savedConfigPath = "savedConfigs/"
+try:
+    os.mkdir("savedConfigs/")
+    print("savedConfig directory created")
+except FileExistsError as error:
+    print("savedConfig directory exists")
 
 def loadMechConfig(configId):
     print("test " + configId)
@@ -8,6 +16,9 @@ def loadMechConfig(configId):
 def saveMechConfig(configId, mech):
     #convert mech model into json format and save to memory
     json_string = json.dumps(getDictHierarchy(mech))
+    save_path = os.path.join(savedConfigPath, configId+".json")
+    mechConfigFile = open(save_path, "w+")
+    mechConfigFile.write(json_string)
 
 def getDictHierarchy(item):
     dict_op = item.__dict__
